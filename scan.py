224b0943b99a26a1acf0b6895aa7005360d77ecf
RoliSoft <rolisoft@gmail.com>
2016-09-11 15:42:16 +0300

    Updated script to Python 3.

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys, requests, socket
@@ -7,15 +7,20 @@ from pprint import pprint
 # Models for the script.
 
 class Site:
-	# Represents a site.
+	"""
+	Represents a site.
+	"""
 
-	def __init__(self, Name, Host, Icon):
+	def __init__(self, Name, Host, Icon, Error = ''):
 		self.Name   = Name
 		self.Host   = Host
 		self.Icon   = Icon
+		self.Error  = Error
 
 class Result:
-	# Represents an endpoint evaluation.
+	"""
+	Represents an endpoint evaluation.
+	"""
 
 	def __init__(self, Site, Grade, SSLv3, TLSv12, SHA1, RC4, PFS, POODLE, Heartbleed, FREAK, Logjam, SCSV, HSTS, EV):
 		self.Site       = Site
@@ -35,7 +40,7 @@ class Result:
 
 # Global variables.
 
-API = "https://api.ssllabs.com/api/v2/"
+QualysAPI = "https://api.ssllabs.com/api/v2/"
 
 Sites = [
 	#     Name                   Hostname                                    Favicon
@@ -62,134 +67,186 @@ Sites = [
 
 # Methods for interacting with the SSL Labs API.
 
-def request(path, payload = {}):
-	# Sends a request to the global endpoint.
+def request(url, payload = {}):
+	"""
+	Sends a request to the global endpoint.
+	"""
 
-	url = API + path
 	response = requests.get(url, params=payload)
 	data = response.json()
 
 	return data
 
 def analyze(host, publish = "off", maxAge = 12, all = "done"):
-	# Starts an analysis on the endpoint, if one was not done within the last maxAge hours.
+	"""
+	Starts an analysis on the endpoint, if one was not done within the last maxAge hours.
+	"""
 
-	path = "analyze"
 	payload = {'host': host, 'publish': publish, 'maxAge': maxAge}
-	data = request(path, payload)
+	data    = request(QualysAPI + 'analyze', payload)
 
-	return data
+	ready  = data['status'] == 'READY'
+
+	if 'statusMessage' in data:
+		status = data['statusMessage']
+	elif 'endpoints' in data:
+		if 'statusDetailsMessage' in data['endpoints'][0]:
+			status = data['endpoints'][0]['statusDetailsMessage']
+		else:
+			status = data['endpoints'][0]['statusMessage']
+	else:
+		status = 'Unknown'
+
+	return ready, status
 
 def getEndpointData(host, s = None):
-	# Fetches the results of the analysis for the specified endpoint.
+	"""
+	Fetches the results of the analysis for the specified endpoint.
+	"""
 
 	if s is None:
 		s = socket.gethostbyname(host)
 
-	path = "getEndpointData"
 	payload = {'host': host, 's': s}
-	data = request(path, payload)
+	data    = request(QualysAPI + 'getEndpointData', payload)
 
 	return data
 
 def info():
-	# Fetches usage information from the API.
+	"""
+	Fetches usage information from the API.
+	"""
 
-	path = "info"
-	data = request(path)
+	data = request(QualysAPI + 'info')
 
 	return data
 
 # Batch scanning helper methods and parser functions.
 
 def parseEndpointObject(site, data):
-	# Parses the Endpoint object returned by the API and extracts the relevant information.
+	"""
+	Parses the Endpoint object returned by the API and extracts the relevant information.
+	"""
+
+	# pprint(data)
 
 	if data.get('progress', 0) != 100:
-		return None
+
+		if 'errors' in data:
+			site.Error = data['errors'][0]['message']
+
+		return site
 
 	return Result(
+
 		# Metadata
 		site, data['grade'],
+
 		# True if server does not support SSLv3
 		not any(prot['name'] == 'SSL' for prot in data['details']['protocols']),
+
 		# True if server supports TLSv1.2
 		any(prot['id'] == 771 for prot in data['details']['protocols']),
+
 		# True if certificate is not signed with SHA1
 		data['details']['cert']['sigAlg'] != 'SHA1withRSA',
+
 		# True if RC4 is not supported
 		not data['details']['supportsRc4'],
+
 		# True if Forward Secrecy is supported with most browsers
 		data['details']['forwardSecrecy'] == 2 or data['details']['forwardSecrecy'] == 4,
+
 		# True if not vulnerable to POODLE
 		not data['details']['poodle'] and data['details']['poodleTls'] != 2,
+
 		# True if not vulnerable to Heartbleed
 		not data['details']['heartbleed'],
+
 		# True if not vulnerable to FREAK
 		not data['details']['freak'],
+
 		# True if not vulnerable to Logjam
 		not data['details'].get('logjam', False),
+
 		# True if server supports TLS_FALLBACK_SCSV
 		data['details'].get('fallbackScsv', False),
+
 		# True if server sends HSTS
 		not not data['details'].get('stsResponseHeader', None),
+
 		# True if cert is EV
 		data['details']['cert'].get('validationType', None) == 'E'
+
 	)
 
 def printTabulated(res):
-	# Prints the values from the specified Result argument into a tab-separated format.
+	"""
+	Prints the values from the specified Result argument into a tab-separated format.
+	"""
 
 	if not hasattr(res, 'Site'):
-		print 'test failed'
+		print('=image("' + res.Icon + '", 4, 16, 16)\t' +
+		      '=hyperlink("https://www.ssllabs.com/ssltest/analyze.html?d=' + res.Host + '","' + res.Name + '")\t' +
+		      '!\t' + res.Error)
 		return
 
-	print '=image("' + res.Site.Icon + '", 4, 16, 16)\t' +\
-		  '=hyperlink("https://www.ssllabs.com/ssltest/analyze.html?d=' + res.Site.Host + '","' + res.Site.Name + '")\t' +\
-		  res.Grade + '\t' +\
-		  ('Fail', 'Pass')[res.SSLv3]      + '\t' +\
-		  ('Fail', 'Pass')[res.TLSv12]     + '\t' +\
-		  ('Fail', 'Pass')[res.SHA1]       + '\t' +\
-		  ('Fail', 'Pass')[res.RC4]        + '\t' +\
-		  ('Fail', 'Pass')[res.PFS]        + '\t' +\
-		  ('Fail', 'Pass')[res.POODLE]     + '\t' +\
-		  ('Fail', 'Pass')[res.Heartbleed] + '\t' +\
-		  ('Fail', 'Pass')[res.FREAK]      + '\t' +\
-		  ('Fail', 'Pass')[res.Logjam]     + '\t' +\
-		  ('Fail', 'Pass')[res.SCSV]       + '\t' +\
-		  ('Fail', 'Pass')[res.HSTS]       + '\t' +\
-		  ('Fail', 'Pass')[res.EV]
+	print('=image("' + res.Site.Icon + '", 4, 16, 16)\t' +
+	      '=hyperlink("https://www.ssllabs.com/ssltest/analyze.html?d=' + res.Site.Host + '","' + res.Site.Name + '")\t' +
+	      res.Grade + '\t' +
+	      ('Fail', 'Pass')[res.SSLv3] + '\t' +
+	      ('Fail', 'Pass')[res.TLSv12] + '\t' +
+	      ('Fail', 'Pass')[res.SHA1] + '\t' +
+	      ('Fail', 'Pass')[res.RC4] + '\t' +
+	      ('Fail', 'Pass')[res.PFS] + '\t' +
+	      ('Fail', 'Pass')[res.POODLE] + '\t' +
+	      ('Fail', 'Pass')[res.Heartbleed] + '\t' +
+	      ('Fail', 'Pass')[res.FREAK] + '\t' +
+	      ('Fail', 'Pass')[res.Logjam] + '\t' +
+	      ('Fail', 'Pass')[res.SCSV] + '\t' +
+	      ('Fail', 'Pass')[res.HSTS] + '\t' +
+	      ('Fail', 'Pass')[res.EV])
 
 # CLI handler methods.
 
 def printUsage():
-	# Prints usage information.
+	"""
+	Prints usage information.
+	"""
 
-	print 'usage: ' + sys.argv[0] + ' [start|info|collect]'
+	print('usage: ' + sys.argv[0] + ' [start|info|collect]')
 
 def printInfo():
-	# Prints usage information from the API.
+	"""
+	Prints usage information from the API.
+	"""
 
 	inf = info()
-	print 'assessments: ' + str(inf['currentAssessments']) + '/' + str(inf['maxAssessments'])
+	print('assessments: ' + str(inf['currentAssessments']) + '/' + str(inf['maxAssessments']))
 
 def startScans():
-	# Starts the scan of all configured hosts.
+	"""
+	Starts the scan of all configured hosts.
+	"""
 
 	for site in Sites:
-		print 'Starting scan of ' + site.Name + '...'
-		analyze(site.Host)
+		print('processing ' + site.Name + ': ', end='', flush=True)
+
+		ready, status = analyze(site.Host)
+
+		if ready:
+			print('Report ready.')
+		else:
+			print(status + '...')
 
 def collectScans():
-	# Collects the analysis results for all configured hosts.
+	"""
+	Collects the analysis results for all configured hosts.
+	"""
 
 	for site in Sites:
-		res  = parseEndpointObject(site, getEndpointData(site.Host))
-
-		if res is None:
-			printTabulated(site)
-		else:
-			printTabulated(res)
+		res = parseEndpointObject(site, getEndpointData(site.Host))
+		printTabulated(res)
 
 # Entry point of the application.
 
